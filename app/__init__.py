4f08848946e18acddededf29998b24d53328d68d
daniyl-x <daniyl-x@proton.me>
2025-10-23 23:36:58 +0300

    Add user config to allow options override

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,16 @@ from flask import Flask
 from app.routes.api import api
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+os.makedirs(app.instance_path, exist_ok=True)
 
-# TODO: add instance settings
-app.config.from_object("app.default_settings")
+app.config.from_object("app.default_config")
+app.config.from_pyfile("config.py", silent=True)
+
 app.config["OUTPUT_PATH"] = os.path.join(
         app.instance_path,
         app.config["OUTPUT_FILENAME"]
         )
 
-os.makedirs(app.instance_path, exist_ok=True)
 
 app.register_blueprint(api, url_prefix="/api")
